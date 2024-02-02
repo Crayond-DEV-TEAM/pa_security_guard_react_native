f3a520efe77c4d96d5e6fe52c8113199cb8c6785
Manojkumar <manojkumar@crayond.co>
2024-02-02 12:54:19 +0530

    feat: :sparkles: Camera and Microphone request permission enabled

--- App.jsx
+++ App.jsx
@@ -6,77 +6,58 @@
 //  * @flow
 //  */
 
-
 // App.jsx
 import React, {useState, useEffect} from 'react';
-import notifee, { AuthorizationStatus } from '@notifee/react-native';
+import notifee, {AuthorizationStatus} from '@notifee/react-native';
 import messaging from '@react-native-firebase/messaging';
-import { onDisplayNotificationFun } from './src/utils/notificationHandler';
-import { firebase } from '@react-native-firebase/app';
+import {onDisplayNotificationFun} from './src/utils/notificationHandler';
+import {firebase} from '@react-native-firebase/app';
 
-import { Platform } from 'react-native';
+import {Platform} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+
+import WebScreen from './src/screen/webScreen';
 import {
-  PermissionsAndroid
-} from 'react-native';
+  requestCameraPermission,
+  requestMicrophonePermission,
+} from './src/utils/accessPermissions';
 
-import WebScreen from "./src/screen/webScreen";
 const App = () => {
-
-  const [token, setToken] = useState("");
-  const [webUrl, setWebUrl] = React.useState("https://dev-security-guard-v2.propertyautomate.com/login");
+  const [token, setToken] = useState('');
+  const [webUrl, setWebUrl] = React.useState(
+    'https://dev-security-guard-v2.propertyautomate.com/login',
+  );
 
   useEffect(() => {
-    if(token){
-    setWebUrl(`https://dev-security-guard-v2.propertyautomate.com/login?deviceToken=${token}`);
+    if (token) {
+      setWebUrl(
+        `https://dev-security-guard-v2.propertyautomate.com/login?deviceToken=${token}`,
+      );
     }
   }, [token]);
 
   useEffect(() => {
     setupNotifications();
-    permission()
+    permission();
   }, []);
 
-  const permission=async()=>{
-
-    const grantedMic = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-      {
-        title: "Audio Permission",
-        message: "App needs access to your audio/microphone",
-        neutralButtonLabel: "Ask Me Later",
-        negativeButtonLabel: "Cancel",
-        positiveButtonLabel: "OK"
-      }
-    );
-  
-    if (grantedMic === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log("You can use the Microphone");
-    } else {
-      console.log("Microphone permission denied");
-    } 
-    
-    
-    let granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.CAMERA,
-      {
-        title: "Camera Permission",
-        message:
-          "App needs access to your camera " +
-          "so others can see you.",
-        buttonNeutral: "Ask Me Later",
-        buttonNegative: "Cancel",
-        buttonPositive: "OK"
-      }
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log("You can use the camera");
-    } else {
-      console.log("Camera permission denied");
-    }
-  
- 
-  }
+  const permission = async () => {
+    requestCameraPermission()
+      .then(async res => {
+        if (res) {
+          console.log('You can use the camera');
+          const hasMicrophoneAccess = await requestMicrophonePermission();
+          if (hasMicrophoneAccess) {
+            console.log('You can use the microphone');
+          } else {
+            console.log('Microphone permission denied');
+          }
+        }
+      })
+      .catch(err => {
+        console.log('Camera permission denied');
+      });
+  };
 
   React.useEffect(() => {
     if (Platform.OS === 'android') {
@@ -85,16 +66,14 @@ const App = () => {
     }
   }, []);
 
-  
-  
   const setupNotifications = async () => {
-    // Check if the app has been granted notification permissions    
-    
+    // Check if the app has been granted notification permissions
+
     const settings = await notifee.getNotificationSettings();
     // await notifee.requestPermission();
-    await messaging().registerDeviceForRemoteMessages();
+    if (Platform.OS === 'android')
+      await messaging().registerDeviceForRemoteMessages();
     setupFCM();
-    
 
     if (settings.authorizationStatus === AuthorizationStatus.NOT_DETERMINED) {
       // Ask for notification permission if it's not determined yet
@@ -110,7 +89,7 @@ const App = () => {
 
   const setupFCM = async () => {
     // Get the FCM token for this device
-    // 
+    //
     const enabled = await firebase.messaging().hasPermission();
     console.log('FCM Token:', token);
     if (enabled) {
@@ -120,61 +99,55 @@ const App = () => {
     }
 
     // Listen for incoming FCM messages when the app is in the foreground
-    messaging().onMessage(async (message) => {
+    messaging().onMessage(async message => {
       // console.log('FCM Message received:', message);
       onDisplayNotificationFun(message);
     });
 
     // Listen for incoming FCM messages when the app is in the background or terminated
-    messaging().setBackgroundMessageHandler(async (message) => {
-        onDisplayNotificationFun(message);
+    messaging().setBackgroundMessageHandler(async message => {
+      onDisplayNotificationFun(message);
     });
 
+    // For handling notification press events in the foreground
+    notifee.onForegroundEvent(async ({type, detail}) => {
+      let url = '';
+      if (detail?.pressAction?.id === 'rejected') {
+        url = `${detail?.notification?.data?.redirect_url}&status=rejected`;
+      } else if (detail?.pressAction?.id === 'accept') {
+        url = `${detail?.notification?.data?.redirect_url}&status=accept`;
+      } else if (type === 1 && !detail?.pressAction?.id) {
+        url = `${detail?.notification?.data?.redirect_url}`;
+      }
+      console.log(url, 'url');
 
-      // For handling notification press events in the foreground
-      notifee.onForegroundEvent(async ({type, detail}) => {
-        let url = '';
-        if (detail?.pressAction?.id === 'rejected') {
-          url = `${detail?.notification?.data?.redirect_url}&status=rejected`;
-        } else if (detail?.pressAction?.id === 'accept') {
-          url = `${detail?.notification?.data?.redirect_url}&status=accept`;
-        } else if (type === 1 && !detail?.pressAction?.id) {
-          url = `${detail?.notification?.data?.redirect_url}`;
-        }
-        console.log(url, 'url');
-  
-        if (url?.length > 0) {
-          setWebUrl(url);
-        }
-  
-        console.log('Notification Press in Foreground:', type);
-      });
-  
-      // For handling notification press events in the background
-      notifee.onBackgroundEvent(async ({type, detail}) => {
-  
-  
-        let url = '';
-        if (detail?.pressAction?.id === 'rejected') {
-          url = `${detail?.notification?.data?.redirect_url}&status='rejected`;
-        } else if (detail?.pressAction?.id === 'accept') {
-          url = `${detail?.notification?.data?.redirect_url}&status='accept`;
-        } else if (type === 1 && !detail?.pressAction?.id) {
-          url = `${detail?.notification?.data?.redirect_url}`;
-        }
-  
-        if (url?.length > 0) {
-          setWebUrl(url);
-        }
-  
-        console.log('Notification Press in background:', type);
-      });
+      if (url?.length > 0) {
+        setWebUrl(url);
+      }
 
+      console.log('Notification Press in Foreground:', type);
+    });
+
+    // For handling notification press events in the background
+    notifee.onBackgroundEvent(async ({type, detail}) => {
+      let url = '';
+      if (detail?.pressAction?.id === 'rejected') {
+        url = `${detail?.notification?.data?.redirect_url}&status='rejected`;
+      } else if (detail?.pressAction?.id === 'accept') {
+        url = `${detail?.notification?.data?.redirect_url}&status='accept`;
+      } else if (type === 1 && !detail?.pressAction?.id) {
+        url = `${detail?.notification?.data?.redirect_url}`;
+      }
+
+      if (url?.length > 0) {
+        setWebUrl(url);
+      }
+
+      console.log('Notification Press in background:', type);
+    });
   };
 
-  return (
-    <WebScreen webUrl={webUrl}/>
-  );
+  return <WebScreen webUrl={webUrl} />;
 };
 
 export default App;
